c629a1d2d15ddf24bf87d2823c7363184bddaeba
MouslyCode <rezasubkhan50@gmail.com>
2026-02-09 19:55:28 +0700

    Barber Service Controller

--- controllers/serviceController.go
+++ controllers/serviceController.go
@@ -1,1 +1,24 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/MouslyCode/bang-cukur/database"
+	"github.com/MouslyCode/bang-cukur/models/serviceModel"
+	"github.com/gin-gonic/gin"
+)
+
+func GetService(c *gin.Context) {
+	var services []serviceModel.Service
+	database.DB.Where("deleted_at IS NULL").Find(&services)
+	c.JSON(http.StatusOK, services)
+}
+
+// func CreateService(c *gin.Context) {
+// 	var input serviceModel.Request
+// 	if err := c.ShouldBindJSON(&input); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// }
